92ff9d943766da008dee9d7223c88571dd6ca94c
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-06-07 16:00:54 +0300

    Finally completed

--- index.js
+++ index.js
@@ -14,29 +14,31 @@
 //     }
 //  console.log(distanceTravelledInFeet(42,42));
 
-function distanceFromHqInBlocks(pickUpLocation){
-    let headQuatre = 42;
-    return Math.abs(headQuatre - pickUpLocation);
+function distanceFromHqInBlocks(pickUpLocation) {
+  let headQuatre = 42;
+  return Math.abs(headQuatre - pickUpLocation);
 }
 console.log(distanceFromHqInBlocks(50));
 
 function distanceFromHqInFeet(pickupLocation) {
-   return distanceFromHqInBlocks(pickupLocation) * 264;
+  return distanceFromHqInBlocks(pickupLocation) * 264;
 }
 
 console.log(distanceFromHqInFeet(50));
 
- function distanceTravelledInFeet (start, destination){
-     return Math.abs((destination - start)*264);
-    }
- console.log(distanceTravelledInFeet(50,60));
-
- function calculatesFarePrice(start, destination) {
-    const distance = Math.abs(destination - start) * 264;
-    if (distance > 2500){
-        return 'cannot travel that far';}
-    else if (distance <= 2500 && distance > 2000){
-        return 25;}
-    else
-        {return distance <= 400 ? 0 : (distance - 400) * 0.02;}
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((destination - start) * 264);
 }
+console.log(distanceTravelledInFeet(50, 60));
+
+
+function calculatesFarePrice(start, destination) {
+    const distance = Math.abs(destination - start) * 264;
+    if (distance > 2500)
+        return 'cannot travel that far';
+    else if (distance <= 2500 && distance > 2000)
+        return 25;
+    else
+        return distance <= 400 ? 0 : (distance - 400) * 0.02;
+}
+
